ac41f15da7ee00d3164e47a91209b5a41b794372
KxaeGyu <babickenan@gmail.com>
2021-11-17 22:18:01 +0100

    NewSolution

---
+++ Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Solution : ISolution
+{
+    public Solution()
+    {
+        // You can initiate and calculate things here
+    }
+
+    /**
+     * Executes a single step of the tank's programming. The tank can only move, turn,
+     * or fire its cannon once per turn. Between each update, the tank's engine remains
+     * running and consumes 1 fuel. This function will be called repeatedly until there
+     * are no more targets left on the grid, or the tank runs out of fuel.
+     */
+    public void Update()
+    {
+        // Write your code here
+    }
+}
