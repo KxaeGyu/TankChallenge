906c18234af871b8e310f3dfe3388998e9203569
KxaeGyu <babickenan@gmail.com>
2021-11-18 21:14:55 +0100

    Kenans Addition in Program

--- Program.cs
+++ Program.cs
@@ -1,220 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
-public class Solution : ISolution
+public class Solution
 {
-    List<EndingAction> actionList;
-    Solution()
+    public Solution()
     {
-        // You can initiate and calculate things here
-        actionList = new List<EndingAction>
-        {
-            EndingAction.TurnLeft,
-            EndingAction.TurnRight,
-            EndingAction.MoveForward
-        };
-    }
-    /**
-     * Executes a single step of the tank's programming. The tank can only move, turn,
-     * or fire its cannon once per turn. Between each update, the tank's engine remains
-     * running and consumes 1 fuel. This function will be called repeatedly until there
-     * are no more targets left on the grid, or the tank runs out of fuel.
-     */
-    EnemyScanner enemyScanner = new EnemyScanner();
-    VehicleMotionHandler driver = new VehicleMotionHandler();
-
-    bool FindAndDestroy()
-    {
-        bool actionTaken = false;
-
-        if (API.IdentifyTarget()) // If enemy within range, shoot
-        {
-            API.FireCannon();
-            actionTaken = true;
-        }
-        if (enemyScanner.isEnemyApproaching()) // Find approaching enemy and face it
-        {
-            CardinalDirection enemyDirection = enemyScanner.GetDirectionNearestEnemy();
-            driver.TurnTo(enemyDirection);
-            actionTaken = true;
-        }
-        return actionTaken;
     }
 
-    enum EndingAction
-    {
-        FireCanon,
-        MoveForward,
-        MoveBackward,
-        TurnRight,
-        TurnLeft
-    }
-
-    internal void Explore()
-    {
-        driver.AutoMoveCW();
-    }
-
-    EndingAction pendingAction;
     public void Update()
     {
-        //We are standing at some pos, need to check if monster is approaching us, in that case we need to face it and kill it.
-        if (!FindAndDestroy())
-        {
-            Explore();
-        }
-        //Find our way around the map.
+        Tank.UpdateDistances();
+        Map.UpdateMap();
+
+        Console.WriteLine("Current position is (x,y): (" + Tank.myPos.x + "," + Tank.myPos.y + ")");
     }
 
-    public class Navigator
+
+}
+// Enumerations
+public enum CardinalDirection : uint
+{
+    North,  //0
+    East,   //1
+    South,  //2
+    West    //3
+}
+
+public enum BlockType
+{
+    Ground,
+    Unidentified,
+    Wall,
+    Hostile
+}
+public static class Tank
+{
+    static int[] movementVector = { 1, 0, -1, 0 };
+
+    public static Vector2 myPos = new Vector2();
+    public static CardinalDirection myDir = CardinalDirection.North;
+
+    //must run first in every loop
+    public static void UpdateDistances()
     {
-        public CardinalDirection currentHeading;
+        Console.WriteLine("Updating Distances");
 
-        public Navigator()
+        int[] distances = new int[] { API.LidarFront(), API.LidarRight(), API.LidarBack(), API.LidarLeft() };
+        int[] shiftedDistances = new int[distances.Length];
+
+        for (int i = 0; i < distances.Length; i++)
         {
-            currentHeading = CardinalDirection.North; // Initial direction always north.
+            shiftedDistances[(i + (int)myDir) % distances.Length] = distances[i];
         }
-
-        public static Navigator Instance
-        {
-            get
-            {
-                if (Instance == null) { Instance = new Navigator(); }
-                return Instance;
-            }
-            private set { }
-        }
-
-        internal CardinalDirection CardinalDirectionRight()
-        {
-            var Arr = (CardinalDirection[])Enum.GetValues(typeof(CardinalDirection));
-            int currentIndex = Arr.Where(d => d == currentHeading).;
-            // use currentHeading and move +1 in array
-            // if at last index, return first
-            return Arr[currentIndex];
-        }
-
-        internal CardinalDirection CardinalDirectionLeft()
-        {
-            var Arr = (CardinalDirection[])Enum.GetValues(typeof(CardinalDirection));
-            int currentIndex = Arr.Select((d, i) => (d, i)).First(d == currentHeading).index;
-            // use currentHeading and move -1 in array.
-            // if at first index, return last
-
-            return Arr[i];
-        }
-
-        internal void TurnRight()
-        {
-            //throw new NotImplementedException();
-        }
-
-        internal void TurnLeft()
-        {
-            //throw new NotImplementedException();
-        }
-
-        internal void MoveForward()
-        {
-            //throw new NotImplementedException();
-        }
+        LidarSamples.north = shiftedDistances[0];
+        LidarSamples.east = shiftedDistances[1];
+        LidarSamples.south = shiftedDistances[2];
+        LidarSamples.west = shiftedDistances[3];
     }
 
-    internal class VehicleMotionHandler
+    public static void TurnLeft()
     {
-        public int LidarFrontTemp;
+        Console.WriteLine("Turning Left");
 
-        internal void AutoMoveCW()
-        {
-            int LidarFrontTemp = API.LidarFront();
-            int LidarLeftTemp = API.LidarLeft();
-            if (LidarFrontTemp == 1)
-            {
-                if (!API.IdentifyTarget())
-                {
-                    TurnRight();
-                }
-            }
-            else if (LidarLeftTemp > 1)
-                {
-                    TurnLeft();
-                }
-            else
-            {
-                MoveForward();
-            }
-        }
+        myDir = (CardinalDirection)(((uint)Tank.myDir - 1) % 4);
+        API.TurnLeft();
 
-        void MoveForward()
-        {
-            Navigator.Instance.MoveForward();
-            API.MoveForward();
-        }
-
-        void TurnRight()
-        {
-            Navigator.Instance.TurnRight();
-            API.TurnRight();
-        }
-
-        internal void TurnLeft()
-        {
-            Navigator.Instance.TurnLeft();
-            API.TurnLeft();
-        }
-
-        public void TurnTo(CardinalDirection cardinalDirection) { }
+        Console.WriteLine("I am facing: " + Tank.myDir);
     }
-
-    internal class EnemyScanner
+    public static void TurnRight()
     {
-        internal CardinalDirection GetDirectionNearestEnemy()
-        {
-            throw new NotImplementedException();
-        }
+        Console.WriteLine("Turning Right");
 
-        internal bool isEnemyApproaching()
-        {
-            throw new NotImplementedException();
-        }
+        myDir = (CardinalDirection)(((uint)myDir + 1) % 4);
+        API.TurnRight();
+
+        Console.WriteLine("I am facing: " + Tank.myDir);
     }
-
-    public enum CardinalDirection
+    public static void MoveForward()
     {
-        North,
-        East,
-        South,
-        West
-    }
+        Console.WriteLine("Moving Forward");
 
-    internal class MapMaker
+        myPos.x += movementVector[((int)myDir + 3) % 4];
+        myPos.y += movementVector[(int)myDir];
+
+        API.MoveForward();
+    }
+    public static void MoveBackward()
     {
-        enum blockTypes
-        {
-            Floor,
-            Unidentified,
-            Wall,
-            Target
-        }
+        Console.WriteLine("Moving Backward");
 
-        int[,] map = new int[1, 1]; //[1] upp/ner [2] vänster/höger
-        int[] myPos = { 0, 0 };
+        myPos.x -= movementVector[((int)myDir + 3) % 4];
+        myPos.y -= movementVector[(int)myDir];
 
-        void ResizeMap()
-        {
-        }
-        //API from High-level code
-        void UpdateMap(int dst_North, int dst_South, int dst_East, int dst_West)
-        {
-        }
-
-        void PrintMap()
-        {
-        }
-
-
-
-
+        API.MoveBackward();
     }
+}
+public static class Map
+{
+    public static BlockType[,] map = new BlockType[1, 1];
+
+    static Vector2 originOffset = new Vector2();
+
+    public static void UpdateMap()
+    {
+    }
+
+
+}
+public static class LidarSamples
+{
+    public static int north { get; set; }
+    public static int east { get; set; }
+    public static int south { get; set; }
+    public static int west { get; set; }
+}
+public class Vector2
+{
+    public int x = 0;
+    public int y = 0;
 }
