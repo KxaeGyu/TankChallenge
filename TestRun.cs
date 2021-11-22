c7250fe7cbb3008d79629d57cb1fb0e3d7ae5e6e
Andreas Larsen <himcor@gmail.com>
2021-11-22 23:24:31 +0100

    Added BuildLevel1() for testing It loads test level1 into the tank map. After loading, set specific tiles to unknown to test Path planner.

--- TestRun.cs
+++ TestRun.cs
@@ -7,7 +7,17 @@ public class TestRun
 {
     static void Main(string[] args)
     {
+        BuildLevel1();
+
+        Map.map2[(-1, 1)].BlockType = BlockType.Unidentified;
+
         
+
+        Map.PrintMap2();
+    }
+
+    private static void SpreadsheetExampleMock()
+    {
         // Step 1
         Console.WriteLine("Step 1");
         LidarSamples.north = 6;
@@ -34,7 +44,7 @@ public class TestRun
         Map.PrintMap2();
 
         Tank.TurnRight();
-        
+
         Console.WriteLine("");
         // Step3
         Console.WriteLine("Step 3");
@@ -42,7 +52,7 @@ public class TestRun
         Map.PrintMap2();
 
         Tank.MoveForward();
-        
+
         Console.WriteLine("");
         // Step 4
         Console.WriteLine("Step 4");
@@ -53,6 +63,47 @@ public class TestRun
 
         Map.UpdateMap2();
         Map.PrintMap2();
+    }
+
+    public static void BuildLevel1()
+    {
+        // Outer walls
+        for (int x = -10; x <= 11; x++)
+        {
+            Map.map2.Add((x, 10), new Tile(x, 10, BlockType.Wall)); // top wall
+            Map.map2.Add((x, -1), new Tile(x, -1, BlockType.Wall)); // bottow wall
+        }
+        for (int y = 0; y <= 9; y++)
+        {
+            Map.map2.Add((-10, y), new Tile(-10, y, BlockType.Wall));
+            Map.map2.Add((11, y), new Tile(11, y, BlockType.Wall));
+        }
+
+        // Initialize everythin withing outer walls as Ground
+        for (int y = 0; y <= 9; y++)
+        {
+            for (int x = -9; x <= 10; x++) 
+            {
+                Map.map2.Add((x, y), new Tile(x, y, BlockType.Ground));
+            }
+        }
+
+        /////// Place inner walls and Hostiles
+        ///
+        // center wall placement
+        for (int x = -7; x <= 8; x++)
+        {
+            Map.map2[(x, 5)].BlockType = BlockType.Wall;
+        }
+        // Hostile placement
+        for (int y = 4; y <= 9; y += 5)
+        {
+            Map.map2[(-6, y)].BlockType = BlockType.Hostile;
+            Map.map2[(-2, y)].BlockType = BlockType.Hostile;
+            Map.map2[(3, y)].BlockType = BlockType.Hostile;
+            Map.map2[(7, y)].BlockType = BlockType.Hostile;
+        }
+
     }
 }
 
